7e8de35fa6503a0240adf374bcdfa8c2da695aa6
inoutlake <lozhkomoyev@sputnikfund.ru>
2025-08-13 10:26:23 +0700

    refactor: DocsManager

--- main/docs_manager.py
+++ main/docs_manager.py
@@ -1,15 +1,37 @@
-from _typeshed import StrPath
+from functools import cache
 import os
-from os import PathLike
 from pathlib import Path
 
 from docsapi.docsapi import DocsAPIProtocol
 from git_tracker import GitTracker
 
 
-class Document(Path):
-    def __init__(self, path: StrPath):
-        super().__init__(path)
+class Document:
+    def __init__(self, path: Path, docs_folder: Path):
+        self.path = path.relative_to(docs_folder)
+        try:
+            self.content = path.read_text()
+            self.template = (
+                self.content.split("\n")[0].replace("<", "").replace(">", "")
+            )
+        except Exception as _:
+            ...
+
+    def create(self, template: str) -> None:
+        self.path.touch(exist_ok=False)
+        self.path.write_text(f"<<{template}>>")
+
+    def edit(self, new_content: str) -> None:
+        self.path.write_text(new_content)
+
+    def delete(self):
+        self.path.unlink()
+
+    def read(self):
+        return self.content
+
+
+class Template(Document): ...
 
 
 class DocsManager:
@@ -30,53 +52,29 @@ class DocsManager:
                 raise Exception("Unknown file type in docs (probably symlink)")
         return files_list
 
-    def list_documents_with_types(self) -> dict[Path, str]:
-        documents_with_types = {}
-        documents_list = self.list_folder(self.docs_folder)
-        for document in documents_list:
-            with open(document, "r") as d:
-                documents_with_types[document] = d.readline()
-        return documents_with_types
+    def list_documents(self) -> list[Document]:
+        paths_list = self.list_folder(self.docs_folder)
+        return [Document(path, self.docs_folder) for path in paths_list]
 
-    def list_documents_with_content(self) -> dict[str, str]:
-        documents_with_content = {}
-        documents_list = self.list_folder(self.docs_folder)
-        for document in documents_list:
-            with open(document, "r") as d:
-                documents_with_content[Path(document).relative_to(self.docs_folder)] = (
-                    d.read()
-                )
-        return documents_with_content
-
-    def sync_document_by_path(self, document_path: Path) -> None:
-        path = Path(self.docs_folder) / document_path
-        with open(path, "r") as f:
-            updated_document = self.docsapi.sync_document_by_path(
-                str(document_path),
-                content=f.read(),
-            )
-        with open(path, "r") as f:
-            f.seek(0)
-            f.write(updated_document.content)
+    def sync_document(self, document: Document) -> None:
+        updated_content = self.docsapi.sync_document_by_path(
+            str(document.path),
+            content=document.read(),
+        )
+        document.edit(updated_content)
 
     def sync_documents(self) -> None:
-        documents = self.list_documents_with_types().keys()
-        documents = [Path(doc).relative_to(self.docs_folder) for doc in documents]
+        documents = self.list_documents()
         for doc in documents:
-            self.sync_document_by_path(doc)
+            self.sync_document(doc)
 
-    def templates(self) -> dict[Path, str]:
-        templates = {}
-        templates_list = self.list_folder(self.templates_folder)
-        for template in templates_list:
-            with open(template, "r") as t:
-                templates[template.read_text] = {
-                    "content": t.read(),
-                }
-        return templates
+    @cache
+    def list_templates(self) -> list[Template]:
+        path_list = self.list_folder(self.templates_folder)
+        return [Template(path, self.templates_folder) for path in path_list]
 
-    def create_document(self, document_path: Path, document_type: str):
-        document_path.write_text(f"<<Template={document_type}>>")
+    def create_document(self, document_path: Path, document_template: str):
+        document_path.write_text(f"<<{document_template}>>")
 
     def edit_document(self, document_path: Path, content: str):
         document_path.write_text(content)
